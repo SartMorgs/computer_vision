2389485200dab332cb873afa12ef6f7ef2e99c8f
Morgana Sartor <morgana.sartor@outlook.com>
2019-03-26 03:34:22 -0300

    Add files via upload

--- Imagem.cpp
+++ Imagem.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 
+using namespace std;
+
+typedef unsigned int u_int;
 typedef struct pixel{
 	int r, g, b, i;
 }PIXEL;
@@ -18,26 +22,30 @@ class Imagem{
 		int getHeigth();
 		void setType(char t[]);
 		char* getType();
-		void setImg(PIXEL* p);
+		void setPGM(PIXEL* pgm);
+		void setPPM(PIXEL* ppm);
+		void setImg(PIXEL* pgm, PIXEL* ppm, int w, int h);
 		PIXEL* getImg();
 
 		// Funções de leitura e escrita
-		void readPGM(FILE* arq, const* char filename);
-		void writePGM(FILE* arq, const* char filename);
-		void readPPM(FILE* arq, const* char filename);
-		void writePPM(FILE* arq, const* char filename);
+		void readPGM(FILE* arq, const char* filename);
+		void writePGM(FILE* arq, const char* filename);
+		void readPPM(FILE* arq, const char* filename);
+		void writePPM(FILE* arq, const char* filename);
 
 		// Funções de escala cinza
-		void thresholdGray(unsigned int th);
-		void thresholdGray(unsigned int thlow, unsigned int thhigh);
+		void thresholdGray(u_int th);
+		void thresholdGray(u_int thlow, u_int thhigh);
 		void inverseGray();
 		void addGray(unsigned char value);
 
 		// Funções de RGB
 		void inverseColor();
 		void addColor(unsigned char value);
-		void addColor(unsigned char valueR, unsigned char valueG, unsigned char value);
+		void addColor(unsigned char valueR, unsigned char valueG, unsigned char valueB);
 		void rgb2Gray();
+		void thresholdRGB(u_int thR, u_int thG, u_int thB);
+		void thresholdRGB(u_int thRlow, u_int thRhigh, u_int thGlow, u_int thGhigh, u_int thBlow, u_int thBhigh);
 };
 
 void Imagem::setWidth(int w){
@@ -57,22 +65,37 @@ int Imagem::getHeigth(){
 }
 
 void Imagem::setType(char t[]){
-	type = t;
+	strcpy(type, t);
 }
 
 char* Imagem::getType(){
 	return type;
 }
 
-void Imagem::setImg(PIXEL* pgm, PIXEL* pgm, int w, int h){
-	
+void Imagem::setPGM(PIXEL* pgm){
+
+    for(unsigned int k = 0; k < (width*heigth); k++){
+        px[k].i = pgm[k].i;
+    }
+}
+
+void Imagem::setPPM(PIXEL* ppm){
+    for(unsigned int k = 0; k < (width*heigth); k++){
+        px[k].r = ppm[k].r;
+        px[k].g = ppm[k].g;
+        px[k].b = ppm[k].b;
+    }
+}
+
+void Imagem::setImg(PIXEL* pgm, PIXEL* ppm, int w, int h){
+
 }
 
 PIXEL* Imagem::getImg(){
 	return px;
 }
 
-void Imagem::readPGM(FILE* arq, const* char filename){
+void Imagem::readPGM(FILE* arq, const char* filename){
 	int w, h, maxsize;
 	char t[2];
 	PIXEL *pxl;
@@ -94,35 +117,29 @@ void Imagem::readPGM(FILE* arq, const* char filename){
 		}
 	}
 
+	setPGM(pxl);
+
 	fclose(arq);
 }
 
 
-void Imagem::writePGM(FILE* arq, const* char filename){
-	int w, h;
-	char t[2];
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-	t = getType();
+void Imagem::writePGM(FILE* arq, const char* filename){
 
 	arq = fopen(filename, "w");
 
 	if(arq == NULL) cout<< "Erro na abertura do arquivo" << "\n";
 	else{
-		fprintf(arq, "%s %d %d 255", t, w, h);
+		fprintf(arq, "%s %d %d 255", type, width, heigth);
 
-		for(unsigned int k = 0; k < (w*h); k++){
-			fprintf(arq, "%d", pxl[k].i);
+		for(unsigned int k = 0; k < (width*heigth); k++){
+			fprintf(arq, "%d", px[k].i);
 		}
 	}
 
 	fclose(arq);
 }
 
-void Imagem::readPPM(FILE* arq, const* char filename){
+void Imagem::readPPM(FILE* arq, const char* filename){
 	int w, h, maxsize;
 	char t[2];
 	PIXEL *pxl;
@@ -144,154 +161,119 @@ void Imagem::readPPM(FILE* arq, const* char filename){
 		}
 	}
 
+	setPPM(pxl);
+
 	fclose(arq);
 }
 
 
-void Imagem::writePPM(FILE* arq, const* char filename){
-	int w, h;
-	char t[2];
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-	t = getType();
+void Imagem::writePPM(FILE* arq, const char* filename){
 
 	arq = fopen(filename, "w");
 
 	if(arq == NULL) cout<< "Erro na abertura do arquivo" << "\n";
 	else{
-		fprintf(arq, "%s %d %d 255", t, w, h);
+		fprintf(arq, "%s %d %d 255", type, width, heigth);
 
-		for(unsigned int k = 0; k < (w*h); k++){
-			fprintf(arq, "%d %d %d", pxl[k].r, pxl[k].g, pxl[k].b);
+		for(unsigned int k = 0; k < (width*heigth); k++){
+			fprintf(arq, "%d %d %d", px[k].r, px[k].g, px[k].b);
 		}
 	}
 
 	fclose(arq);
 }
 
-void thresholdGray(unsigned int th){
-	int w, h;
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		if((unsigned int) pxl[k].i < th) pxl[k].i = 255;
-		else pxl[k].i = 0;
+void Imagem::thresholdGray(unsigned int th){
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		if((unsigned int) px[k].i < th) px[k].i = 255;
+		else px[k].i = 0;
 	}
 }
 
-void thresholdGray(unsigned int thlow, unsigned int thhigh){
-	int w, h;
-	PIXEL *pxl;
+void Imagem::thresholdGray(unsigned int thlow, unsigned int thhigh){
 
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		if(((unsigned int) pxl[k].i > thlow) && ((unsigned int) pxl[k].i < thhigh)) pxl[k].i = 255;
-		else pxl[k].i = 0;
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		if(((unsigned int) px[k].i > thlow) && ((unsigned int) px[k].i < thhigh)) px[k].i = 255;
+		else px[k].i = 0;
 	}
 }
 
-void inverseGray(){
-	int w, h;
-	PIXEL *pxl;
+void Imagem::inverseGray(){
 
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		pxl[k].i = 255 - pxl[k].i;
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		px[k].i = 255 - px[k].i;
 	}
 }
 
-void addGray(unsigned char value){
-	int w, h;
-	PIXEL *pxl;
+void Imagem::addGray(unsigned char value){
 
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		if((int) (pxl[k].i + value) > 255) pxl[k].i = 255;
-        else pxl[k].i += value;
-	}	
-}
-
-void inverseColor(){
-	int w, h;
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		pxl[k].r = 255 - pxl[k].r;
-		pxl[k].g = 255 - pxl[k].g;
-		pxl[k].b = 255 - pxl[k].b;
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		if((int) (px[k].i + value) > 255) px[k].i = 255;
+        else px[k].i += value;
 	}
 }
 
-void addColor(unsigned char value){
-	int w, h;
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		if((int) (pxl[k].r + value) > 255) pxl[k].r = 255;
-        else pxl[k].r += value;
-        if((int) (pxl[k].g + value) > 255) pxl[k].g = 255;
-        else pxl[k].g += value;
-        if((int) (pxl[k].b + value) > 255) pxl[k].b = 255;
-        else pxl[k].b += value;
+void Imagem::inverseColor(){
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		px[k].r = 255 - px[k].r;
+		px[k].g = 255 - px[k].g;
+		px[k].b = 255 - px[k].b;
 	}
 }
 
-void addColor(unsigned char valueR, unsigned char valueG, unsigned char value){
-	int w, h;
-	PIXEL *pxl;
-
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
-
-	for(unsigned int k = 0; k < (w*h); k++){
-		if((int) (pxl[k].r + valueR) > 255) pxl[k].r = 255;
-        else pxl[k].r += valueR;
-
-        if((int) (pxl[k].g + valueG) > 255) pxl[k].g = 255;
-        else pxl[k].g += valueG;
-
-        if((int) (pxl[k].b + valueB) > 255) pxl[k].b = 255;
-        else pxl[k].b += valueB;
+void Imagem::addColor(unsigned char value){
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		if((int) (px[k].r + value) > 255) px[k].r = 255;
+        else px[k].r += value;
+        if((int) (px[k].g + value) > 255) px[k].g = 255;
+        else px[k].g += value;
+        if((int) (px[k].b + value) > 255) px[k].b = 255;
+        else px[k].b += value;
 	}
 }
 
-void rgb2Gray(){
-	int w, h;
-	PIXEL *pxl;
-	unsigned int value
+void Imagem::addColor(unsigned char valueR, unsigned char valueG, unsigned char valueB){
 
-	pxl = getImg();
-	w = getWidth();
-	h = getHeigth();
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		if((int) (px[k].r + valueR) > 255) px[k].r = 255;
+        else px[k].r += valueR;
 
-	for(unsigned int k = 0; k < (w*h); k++){
-		value = ((unsigned int) pxl[k].r + pxl[k].g + pxl[k].b) / 3;
-        pxl[k].i = value;
+        if((int) (px[k].g + valueG) > 255) px[k].g = 255;
+        else px[k].g += valueG;
+
+        if((int) (px[k].b + valueB) > 255) px[k].b = 255;
+        else px[k].b += valueB;
+	}
+}
+
+void Imagem::rgb2Gray(){
+	unsigned int value;
+
+	for(unsigned int k = 0; k < (width*heigth); k++){
+		value = ((unsigned int) px[k].r + px[k].g + px[k].b) / 3;
+        px[k].i = value;
+	}
+}
+
+void Imagem::thresholdRGB(u_int thR, u_int thG, u_int thB){
+    for(u_int k = 0; k < (width*heigth); k++){
+		if((u_int) px[k].r < thR) px[k].r = 255;
+		else px[k].r = 0;
+		if((u_int) px[k].g < thG) px[k].g = 255;
+		else px[k].g = 0;
+		if((u_int) px[k].b < thB) px[k].b = 255;
+		else px[k].b = 0;
+	}
+}
+
+void Imagem::thresholdRGB(u_int thRlow, u_int thRhigh, u_int thGlow, u_int thGhigh, u_int thBlow, u_int thBhigh){
+    for(u_int k = 0; k < (width*heigth); k++){
+		if(((u_int) px[k].r > thRlow) && ((u_int) px[k].r < thRhigh)) px[k].r = 255;
+		else px[k].r = 0;
+		if(((u_int) px[k].g > thGlow) && ((u_int) px[k].g < thGhigh)) px[k].g = 255;
+		else px[k].g = 0;
+		if(((u_int) px[k].b > thBlow) && ((u_int) px[k].b < thBhigh)) px[k].b = 255;
+		else px[k].b = 0;
 	}
 }
 
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Imagem.cpp"
+
+using namespace std;
+
+Imagem img;
+
+int main(){
+	FILE *ppm_file, *pgm_file;
+	PIXEL *p;
+	int index;
+
+	p = NULL;
+
+    cout<< "INICIO DO PROGRAMA!" << "\n";
+
+	img.readPGM(pgm_file, "apple.pgm");
+	img.readPPM(ppm_file, "apple.ppm");
+
+	p = img.getImg();
+
+	if(p == NULL) cout << "VAZIO!" << "\n";
+
+	cout << "TAMANHO DA IMAGEM: " << img.getHeigth() << " x " << img.getWidth() << "\n";
+
+	cout << "IMAGEM PGM:" << "\n";
+	index = 0;
+	for(unsigned int k = 0; k < (img.getWidth()*img.getHeigth()); k++){
+		cout << p[k].i << "\t";
+		if((k / img.getWidth()) > index){
+			cout << "\n";
+			index = k / img.getWidth();
+		}
+	}
+
+	cout << "IMAGEM PPM:" << "\n";
+	index = 0;
+	for(unsigned int k = 0; k < (img.getWidth()*img.getHeigth()); k++){
+		cout << p[k].r << " " << p[k].g << " " << p[k].b << " " << "\t";
+		if((k / img.getWidth()) > index){
+			cout << "\n";
+			index = k / img.getWidth();
+		}
+	}
+
+	return 0;
+}
